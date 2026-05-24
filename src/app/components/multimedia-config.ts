bc20bf33ba8608d4c07faf0a8d0a19e4f1195086
11diwgo <naharroalvir11@gmail.com>
2026-05-24 22:37:18 +0200

    SECC

--- src/app/components/multimedia-config.ts
+++ src/app/components/multimedia-config.ts
@@ -50,6 +50,7 @@ export const MULTIMEDIA_CATEGORIES: MediaCategory[] = [
     folder: "menus",
     items: [
        { file: "menu1.png", caption: "Menú de modalidades" },
+       { file: "menu2.png", caption: "Menú de warps del servidor" },
     ],
   },
   {
